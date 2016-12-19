69ab975a6ff3d03d0ed32029aff31d0ad72d99ec
Saju Madhavan <sajuptpm@gmail.com>
2016-12-19 15:58:33 +0530

    save end_time in zookeepr

--- flowlogd/constants.py
+++ flowlogd/constants.py
@@ -1,2 +1,3 @@
 CONFIG_FILENAME = '/etc/flowlogd/flowlogd.cfg'
 LOG_FILENAME = '/var/log/flowlogd/flowlog.log'
+ZK_FLOWLOG_PATH = '/flowlog/accounts/{acc_id}'
--- flowlogd/tasks.py
+++ flowlogd/tasks.py
@@ -1,14 +1,14 @@
+import json
+import time
 import celery
 from celery import Celery
 from celery import group
-import time
-import random
-import socket
+import kazoo.client
 import zkcelery
 import ConfigParser
-from put_flow_logs import get_logs, get_log_enable_account_ids
 import utils
 import constants
+from put_flow_logs import get_logs, get_log_enable_account_ids
 
 LOG = utils.get_logger()
 config = ConfigParser.ConfigParser()
@@ -18,30 +18,81 @@ periodic_task_interval = int(config.get('task', 'periodic_task_interval', 300))
 
 app = Celery('tasks', backend='rpc://', broker=broker_url)
 
-@app.task(base=zkcelery.LockTask, bind=True)
+
+class FlowlogTask(zkcelery.LockTask):
+
+    def get_or_create_node(self, path, value='', acl=None,
+                        ephemeral=False, sequence=False, makepath=False):
+        client = None
+        hosts = getattr(self.app.conf, 'ZOOKEEPER_HOSTS', '127.0.0.1:2181')
+        try:
+            client = kazoo.client.KazooClient(hosts=hosts)
+            client.start()
+            if not client.exists(path):
+                client.create(path, value=value, acl=acl, ephemeral=ephemeral,
+                            sequence=sequence, makepath=makepath)
+            return client.get(path)
+        except Exception as ex:
+            raise ex
+        finally:
+            if client:
+                client.stop()
+                client.close()
+
+    def set_value(self, path, value):
+        client = None
+        hosts = getattr(self.app.conf, 'ZOOKEEPER_HOSTS', '127.0.0.1:2181')
+        try:
+            client = kazoo.client.KazooClient(hosts=hosts)
+            client.start()
+            client.set(path, value)
+        except Exception as ex:
+            raise ex
+        finally:
+            if client:
+                client.stop()
+                client.close()
+
+
+@app.task(base=FlowlogTask, bind=True)
 def flow_log_periodic_task(self):
     with self.lock() as lock:
         if lock:
             LOG.info("Submitting tasks to collect flowlog for accounts")
             acc_ids = get_log_enable_account_ids()
             for acc_id in acc_ids:
-                process_flowlog.apply_async(args=[acc_id])
+                start_time = None
+                path = constants.ZK_FLOWLOG_PATH.format(acc_id=acc_id)
+                node_data = self.get_or_create_node(path, makepath=True)
+                if node_data and isinstance(node_data, tuple):
+                    data = node_data[0]
+                    data = json.loads(data)
+                    if data and isinstance(data, dict):
+                        start_time = data.get('end_time')
+                process_flowlog.apply_async(args=[acc_id],
+                                            kwargs={'start_time': start_time})
+                LOG.info("Submitted task to collect flowlog for account:{acc_id}, start_time:{start_time}".\
+                    format(acc_id=acc_id, start_time=start_time))
             LOG.info("Submitted tasks to collect flowlog for accounts")
             time.sleep(periodic_task_interval)#To avoid periodic task overlap
         else:
             LOG.info("Periodic task already running on another node")
 
-@app.task(base=zkcelery.LockTask, bind=True)
-def process_flowlog(self, acc_id):
+
+@app.task(base=FlowlogTask, bind=True)
+def process_flowlog(self, acc_id, start_time=None):
     with self.lock(acc_id) as lock:
         if lock:
             LOG.info("Collecting flowlog for account:{acc_id}".format(acc_id=acc_id))
-            get_logs(acc_id)
+            end_time = get_logs(acc_id)
+            path = constants.ZK_FLOWLOG_PATH.format(acc_id=acc_id)
+            node_data = json.dumps({'end_time':'19-12-2016 14:48:58'})
+            self.set_value(path, node_data)
             LOG.info("Collected flowlog for account:{acc_id}".format(acc_id=acc_id))
         else:
             LOG.info("Task for account:{acc_id} already running on another node".format(acc_id=acc_id))
 
+
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(periodic_task_interval, flow_log_periodic_task.s())
-
